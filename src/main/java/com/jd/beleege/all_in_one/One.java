56accfefd4945d1ac79336112b873d87f84d4eda
zhangxiaolong31 <zhangxiaolong3@jd.com>
2017-02-22 17:28:26 +0800

    添加基本骨架

---
+++ src/main/java/com/jd/beleege/all_in_one/One.java
@@ -0,0 +1,11 @@
+package com.jd.beleege.all_in_one;
+
+/**
+ * Created by apple on 2017/2/22.
+ */
+public class One {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
